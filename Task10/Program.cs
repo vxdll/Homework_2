c6f46135882a186865a741c3387d506ec3866702
Григорий Васякин <vxdll.39@gmail.com>
2022-09-08 21:52:26 +0200

    Task 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-Console.WriteLine("Введите трёхзначное число");
+Console.WriteLine("Введите трёхзначное число:");
 int number = Convert.ToInt32(Console.ReadLine());
 string secondNum = Convert.ToString(number);
 Console.WriteLine("Вторая цифра числа: " + secondNum[1]);
