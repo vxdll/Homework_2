e6921d968ce3284f54c85a9f81d99b8627576516
Григорий Васякин <vxdll.39@gmail.com>
2022-09-08 21:53:13 +0200

    Task 13

--- Task 13/Program.cs
+++ Task 13/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-Console.WriteLine("Введите число");
+Console.WriteLine("Введите число:");
 int number = Convert.ToInt32(Console.ReadLine());
 string thirdNum = Convert.ToString(number);
 if(thirdNum.Length > 2)
